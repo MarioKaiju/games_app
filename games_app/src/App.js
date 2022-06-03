1a2925075a37fe3c1ac368f83b46eb88b324b33f
MarioKaiju <marioavazquez2011@hotmail.com>
2022-06-02 19:32:31 -0500

    Iniciado el estilado del frontend, creación de la barra de navegación y el enrutador de react-router-dom

--- games_app/src/App.js
+++ games_app/src/App.js
@@ -1,8 +1,22 @@
 import GamesList from "./components/GamesList";
+import NavBar from "./components/NavBar";
+import styled from "styled-components";
+import { Route, Routes } from 'react-router-dom'
+
+const Container = styled.div`
+  margin: 50px 5%;
+`;
 
 function App() {
   return (
-    <GamesList />
+    <>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/games" element={<GamesList />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
